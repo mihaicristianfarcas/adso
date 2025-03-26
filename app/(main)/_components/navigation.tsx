c69260100318da0dd4d6f6923380b0d510386855
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-03-26 17:22:53 +0200

    feat: recursive document list in sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -12,10 +12,11 @@ import { usePathname } from 'next/navigation'
 import { useEffect, useRef, useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
 import UserItem from './user-item'
-import { useMutation, useQuery } from 'convex/react'
+import { useMutation } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 import Item from './item'
 import { toast } from 'sonner'
+import DocumentList from './document_list'
 
 const Navigation = () => {
   const pathname = usePathname()
@@ -27,7 +28,6 @@ const Navigation = () => {
   const [isResetting, setIsResetting] = useState(false)
   const [isCollapsed, setIsCollapsed] = useState(isMobile)
 
-  const documents = useQuery(api.documents.get)
   const create = useMutation(api.documents.create)
 
   useEffect(() => {
@@ -146,9 +146,7 @@ const Navigation = () => {
           />
         </div>
         <div className='mt-4'>
-          {documents?.map(document => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
         <div
           onMouseDown={handleMouseDown}
