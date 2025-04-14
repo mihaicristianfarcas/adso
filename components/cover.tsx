fc0ccd3d6a06d3c18a457feee80eb2ed918ca713
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-04-14 11:40:12 +0300

    feat: change and remove cover works

--- components/cover.tsx
+++ components/cover.tsx
@@ -2,6 +2,13 @@
 
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
+import { Button } from './ui/button'
+import { ImageIcon, X } from 'lucide-react'
+import { useCoverImage } from '@/hooks/use-cover-image'
+import { useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { useParams } from 'next/navigation'
+import { Id } from '@/convex/_generated/dataModel'
 
 interface CoverImageProps {
   url?: string
@@ -9,6 +16,14 @@ interface CoverImageProps {
 }
 
 export const Cover = ({ url, preview }: CoverImageProps) => {
+  const params = useParams()
+  const coverImage = useCoverImage()
+  const removeCoverImage = useMutation(api.documents.removeCoverImage)
+
+  const onRemove = () => {
+    removeCoverImage({ id: params.documentId as Id<'documents'> })
+  }
+
   return (
     <div
       className={cn(
@@ -18,6 +33,28 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
       )}
     >
       {!!url && <Image src={url} fill alt='cover' className='object-cover' />}
+      {url && !preview && (
+        <div className='absolute right-5 bottom-5 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100'>
+          <Button
+            onClick={coverImage.onOpen}
+            className='text-muted-foreground text-xs'
+            variant='secondary'
+            size='sm'
+          >
+            <ImageIcon className='mr-2 h-4 w-4' />
+            Change cover
+          </Button>
+          <Button
+            onClick={onRemove}
+            className='text-muted-foreground text-xs'
+            variant='secondary'
+            size='sm'
+          >
+            <X className='mr-2 h-4 w-4' />
+            Remove
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
