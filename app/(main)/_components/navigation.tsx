3dd7a04f31aaa6e5282b673f3adbe0a6834f536a
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-04-15 14:56:46 +0300

    renamed trash_box to trash-box

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -24,7 +24,7 @@ import {
   PopoverContent,
   PopoverTrigger
 } from '@/components/ui/popover'
-import { TrashBox } from './trash_box'
+import { TrashBox } from './trash-box'
 import { useSearch } from '@/hooks/use-search'
 import { useSettings } from '@/hooks/use-settings'
 import { Navbar } from './navbar'
